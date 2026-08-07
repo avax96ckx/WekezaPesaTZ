eaea734d86699dbc51aac9ceffce649ceb6bfa85
AVAX -FX <icx755@gmail.com>
2026-08-07 19:19:57 +0300

    Switch to @vitejs/plugin-react-swc and update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,9 @@
-import path from "path";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-  },
   plugins: [react()],
-  build: {
-    outDir: "dist",
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
